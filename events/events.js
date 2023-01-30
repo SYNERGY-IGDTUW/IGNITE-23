3ceacb5c7d12975b0c98a8e5bd7a13c47b332b29
Anushri Poonia <pooniaanushri1@gmail.com>
2023-01-30 21:24:51 +0530

    changes

--- events/events.js
+++ events/events.js
@@ -1,17 +1,19 @@
 function football(){
-    location.href = "https://www.youtube.com/watch?v=hzOvS5C1Fw8";
+  location.href = "";
 }
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
+hamburger.classList.toggle("active");
+navMenu.classList.toggle("active");
 });
 
 document.querySelectorAll(".nav-link").forEach((link) =>
-  link.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-  })
+link.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+})
 );
+
+
